724748600e9adafd2f2c41c933c2a2bc6e703c4e
lauraholguin0 <109982217+lauraholguin0@users.noreply.github.com>
2025-06-10 19:01:20 -0400

    Add files via upload

---
+++ entrenar_modelo.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+import os
+
+# Ruta del dataset
+DATASET_DIR = "Flora_parque_ambrosio"
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+
+# Cargar dataset
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    DATASET_DIR,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    DATASET_DIR,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE
+)
+
+class_names = train_ds.class_names
+print("Clases detectadas:", class_names)
+
+# Optimizar lectura
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+
+# Modelo CNN
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(1./255, input_shape=(224, 224, 3)),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(64, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(128, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(len(class_names), activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Entrenar modelo
+model.fit(train_ds, validation_data=val_ds, epochs=10)
+
+# Guardar modelo y clases
+model.save("modelo_clasificador_flora.keras")
+
+# Guardar clases en archivo de texto
+with open("clases.txt", "w") as f:
+    for clase in class_names:
+        f.write(clase + "\n")
